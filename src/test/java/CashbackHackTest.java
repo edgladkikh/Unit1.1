7bfef33e94d6b85b3dd4a248b5fe01f0eae56695
EkaterinaGladkikh <hedgehog96174@gmail.com>
2025-05-06 22:35:07 +0300

    add TestNG to dependencies

---
+++ src/test/java/CashbackHackTest.java
@@ -0,0 +1,28 @@
+import org.testng.annotations.Test;
+import ru.netology.service.CashbackHackService;
+
+import static org.testng.Assert.assertEquals;
+
+public class CashbackHackTest {
+    @Test
+    public void shouldCalculateAmountBeforeCashback() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 900;
+
+        int actual = service.remain(amount);
+        int expected = 100;
+
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void shouldNotOfferToIncreaseAmountSales() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1000;
+
+        int actual = service.remain(amount);
+        int expected = 0;
+
+        assertEquals(actual, expected);
+    }
+}
